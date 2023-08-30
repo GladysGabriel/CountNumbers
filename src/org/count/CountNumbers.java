ad05870a8ef8a73dc44f077ec617f0e4bc8cd9d0
GladysGabriel <lgssujithra@gmail.com>
2023-08-29 20:10:14 -0700

    FirstCommit

---
+++ src/org/count/CountNumbers.java
@@ -0,0 +1,11 @@
+package org.count;
+
+public class CountNumbers {
+
+	public static void main(String[] args) {
+		int count=0,num=123;
+		for(;num !=0; num/=10, count++) {	
+		}
+		System.out.println("Number of digits: "+count);
+	}
+}
